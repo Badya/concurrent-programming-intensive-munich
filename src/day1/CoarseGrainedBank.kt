4606b5cfe0992d44c341b33f07d04908d74bd8de
badya <bogdan.mukvich@gmail.com>
2023-08-07 17:28:09 +0200

    Impl: Coarse-Grained bank

--- src/day1/CoarseGrainedBank.kt
+++ src/day1/CoarseGrainedBank.kt
@@ -4,6 +4,7 @@ package day1
 
 import day1.Bank.Companion.MAX_AMOUNT
 import java.util.concurrent.locks.*
+import kotlin.concurrent.withLock
 
 class CoarseGrainedBank(accountsNumber: Int) : Bank {
     private val accounts: Array<Account> = Array(accountsNumber) { Account() }
@@ -13,25 +14,31 @@ class CoarseGrainedBank(accountsNumber: Int) : Bank {
 
     override fun getAmount(id: Int): Long {
         // TODO: Make this operation thread-safe via coarse-grained locking.
-        return accounts[id].amount
+        return globalLock.withLock {
+            accounts[id].amount
+        }
     }
 
     override fun deposit(id: Int, amount: Long): Long {
-        // TODO: Make this operation thread-safe via coarse-grained locking.
         require(amount > 0) { "Invalid amount: $amount" }
+        // TODO: Make this operation thread-safe via coarse-grained locking.
         val account = accounts[id]
-        check(!(amount > MAX_AMOUNT || account.amount + amount > MAX_AMOUNT)) { "Overflow" }
-        account.amount += amount
-        return account.amount
+        return globalLock.withLock {
+            check(!(amount > MAX_AMOUNT || account.amount + amount > MAX_AMOUNT)) { "Overflow" }
+            account.amount += amount
+            account.amount
+        }
     }
 
     override fun withdraw(id: Int, amount: Long): Long {
         // TODO: Make this operation thread-safe via coarse-grained locking.
         require(amount > 0) { "Invalid amount: $amount" }
         val account = accounts[id]
-        check(account.amount - amount >= 0) { "Underflow" }
-        account.amount -= amount
-        return account.amount
+        return globalLock.withLock {
+            check(account.amount - amount >= 0) { "Underflow" }
+            account.amount -= amount
+            account.amount
+        }
     }
 
     override fun transfer(fromId: Int, toId: Int, amount: Long) {
@@ -40,10 +47,12 @@ class CoarseGrainedBank(accountsNumber: Int) : Bank {
         require(fromId != toId) { "fromIndex == toIndex" }
         val from = accounts[fromId]
         val to = accounts[toId]
-        check(amount <= from.amount) { "Underflow" }
-        check(!(amount > MAX_AMOUNT || to.amount + amount > MAX_AMOUNT)) { "Overflow" }
-        from.amount -= amount
-        to.amount += amount
+        globalLock.withLock {
+            check(amount <= from.amount) { "Underflow" }
+            check(!(amount > MAX_AMOUNT || to.amount + amount > MAX_AMOUNT)) { "Overflow" }
+            from.amount -= amount
+            to.amount += amount
+        }
     }
 
     /**
